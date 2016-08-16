3d8276baa5e792f83d0129141a9da0631d4ffd09
Johanna Wiel <jwiel86@gmail.com>
2016-08-16 12:11:42 -0400

    added logic to only send emails to authroized user for the delete and add action if a SSR for SR have non-draft status

--- lib/dashboard/associated_user_creator.rb
+++ lib/dashboard/associated_user_creator.rb
@@ -8,6 +8,15 @@ module Dashboard
       protocol = Protocol.find(params[:protocol_id])
       @protocol_role = protocol.project_roles.build(params)
 
+      # Iterate through ssr's to collect status'
+      statuses = []
+      protocol.sub_service_requests.each do |ssr|
+        statuses << ssr.status
+      end
+
+      # Do not send email if all statuses for protocol are 'draft'
+      send_email = !(statuses.uniq.length == 1 && statuses.first == "draft")
+
       if @protocol_role.unique_to_protocol? && @protocol_role.fully_valid?
         @successful = true
         if @protocol_role.role == 'primary-pi'
@@ -18,7 +27,9 @@ module Dashboard
         
         @protocol_role.save
 
-        if SEND_AUTHORIZED_USER_EMAILS
+        # Send emails if SEND_AUTHORIZED_USER_EMAILS is set to true and 
+        # if there are any non-draft SSRs
+        if SEND_AUTHORIZED_USER_EMAILS && send_email
           protocol.emailed_associated_users.each do |project_role|
             UserMailer.authorized_user_changed(project_role.identity, protocol, modified_user, action).deliver unless project_role.identity.email.blank?
           end
